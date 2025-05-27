63e15d9c4643e8245e8d66c84126d28731f03861
shaikh-rafik <friendwithmam2019@gmail.com>
2025-05-27 15:31:56 +0530

    add image

--- src/data/healthStatus.js
+++ src/data/healthStatus.js
@@ -1,7 +1,7 @@
  const healthStatusData = [
-    { iconSrc: "lungs.png", label: "Lungs", date: "26 Oct 2021", color: "black" },
-    { iconSrc: "teeth.png", label: "Teeth", date: "26 Oct 2021", color: "black" },
-    { iconSrc: "bone.png", label: "Bone", date: "26 Oct 2021", color: "black" },
+    { iconSrc: "image/lungs.png", label: "Lungs", date: "26 Oct 2021", color: "black" },
+    { iconSrc: "image/teeth.png", label: "Teeth", date: "26 Oct 2021", color: "black" },
+    { iconSrc: "iamge/bone.png", label: "Bone", date: "26 Oct 2021", color: "black" },
   ];
 
   export default  healthStatusData;
